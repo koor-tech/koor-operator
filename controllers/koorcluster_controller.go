4a2edaf93658d89fe2a1a7c8af153f1f07a94a43
Zuhair AlSader <zuhair@koor.tech>
2023-05-10 23:12:56 -0400

    feat: add notification options
    Signed-off-by: Zuhair AlSader <zuhair@koor.tech>

--- controllers/koorcluster_controller.go
+++ controllers/koorcluster_controller.go
@@ -60,10 +60,6 @@ type KoorClusterReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the KoorCluster object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
 //
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.13.0/pkg/reconcile
